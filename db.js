7e939a6ab56f2c8eb259189b65ca1b7f0f0c429b
Sumit-Lohar05 <loharsumit100@gmail.com>
2025-12-14 20:40:00 +0530

    Fix: Enabled SSL for secure cloud MySQL connection to resolve deployment error.

--- db.js
+++ db.js
@@ -4,12 +4,19 @@ const db = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
+
+  ssl: {
+    rejectUnauthorized: true, 
+  }
 });
 
 db.connect(err => {
-  if (err) throw err;
-  console.log('MySQL Connected...');
+  if (err) {
+    console.error('MySQL connection failed:', err.code);
+    throw err; 
+  }
+  console.log('MySQL (Secure) Connected...');
 });
 
 module.exports = db;
